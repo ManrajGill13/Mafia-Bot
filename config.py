64a610b4f2be2c35f80518266100c3e23ffff371
Manraj <manraj836@gmail.com>
2020-09-06 22:05:37 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-token = 'NzQzMzIxNTAwMjMyMzE5MDM2.XzS-Fw.SNg0t_uNbqZSFLaxE867GlO_3nE'
+token = 'Token'
 prefix = 'm!'    
 # webkey = '5aaeb56fc6msh60f4ca4473ffd9dp1f625fjsna9fae701d847'
 rules = "rules.txt"
